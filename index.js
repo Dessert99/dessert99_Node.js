51a2ba37213c96447b072cf955bc53e07cd17565
Dessert99 <lsjh1234@naver.com>
2025-09-24 16:50:37 +0900

    Chore: index.js prettier적용

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-require("dotenv").config() // 환경변수
-const express = require('express')
-const mongoose = require('mongoose')
-const app = express()
-const PORT = 8080
+require('dotenv').config(); // 환경변수
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const PORT = 8080;
 
-mongoose.connect(process.env.MONGO_URI).then(()=>console.log('MongoDB 연결 완료')).catch((error)=>console.log("MongoDB와 연결에 실패했습니다.",error))
-    
-app.listen(PORT, ()=>{
-    console.log(`http://localhost:${PORT} <- 클릭`)
-})
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB 연결 완료'))
+  .catch((error) => console.log('MongoDB와 연결에 실패했습니다.', error));
 
-app.get('/', (req,res)=>{
-    res.send('hello')
-})
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT} <- 클릭`);
+});
 
-
+app.get('/', (req, res) => {
+  res.send('hello');
+});
