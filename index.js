cca0268da307d61c69b0956d8de2a913168eab91
Dessert99 <lsjh1234@naver.com>
2025-09-24 16:36:57 +0900

    Build: 몽고DB 연결 완료

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+require("dotenv").config() // 환경변수
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 const PORT = 8080
 
+mongoose.connect(process.env.MONGO_URI).then(()=>console.log('MongoDB 연결 완료')).catch((error)=>console.log("MongoDB와 연결에 실패했습니다.",error))
+    
 app.listen(PORT, ()=>{
     console.log(`http://localhost:${PORT} <- 클릭`)
 })
@@ -9,4 +13,5 @@ app.listen(PORT, ()=>{
 app.get('/', (req,res)=>{
     res.send('hello')
 })
+
 
