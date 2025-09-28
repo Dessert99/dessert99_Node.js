ad0ad6fe1ac82645a2d4ff90ad23dc63bf03ade9
Dessert99 <lsjh1234@naver.com>
2025-09-28 23:25:07 +0900

    Feat: 관리자 라우터 구현

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const mongoose = require('mongoose');
 const app = express();
 const PORT = 8080;
 
+app.use(express.json()); //Content-Type: application/json 요청의 본문을 파싱해서 req.body에 JS 객체로 넣어준다.
+app.use(express.urlencoded()); //Content-Type: application/x-www-form-urlencoded(일반 HTML 폼 전송) 본문을 파싱해서 req.body에 넣어준다.
+
+const userRoutes = require('./routes/user'); //유저 라우터 가져오기
+
+app.use('/api/auth', userRoutes); // 라우팅
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB 연결 완료'))
---
+++ routes/user.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const User = require('../models/User');
+
+router.post('/signup', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    // DB에서 중복된 유저 있는지 확인 로직
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(400).json({ message: '이미 존재하는 사용자입니다.' });
+    }
+
+    // 패스워드 암호화
+    const hashedPassword = await bcrypt.hash(password, 10); // 10진수로 해시화
+
+    // 회원가입 모델 만들기: 유저네임, 비밀번호 외에는 디폴트 값으로 저장
+    const user = new User({
+      username: username,
+      password: hashedPassword,
+    });
+
+    // 몽고 디비에 저장
+    await user.save();
+    res.status(201).json({ message: '회원가입이 완료되었습니다.' });
+  } catch (error) {
+    res.status(500).json({ message: '서버 오류가 발생했습니다.' });
+    console.log(error);
+  }
+});
+
+module.exports = router;
